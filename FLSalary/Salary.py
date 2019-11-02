26e82bfbe10ba05fca2caa35ea82adc7db57e351
Jarosław Drzaszcz <s16136@pjwstk.edu.pl>
2019-11-02 11:12:43 +0100

    second  commit

---
+++ FLSalary/Salary.py
@@ -0,0 +1,16 @@
+class Salary:
+    experience = None
+    work_group = None
+    education_level = None
+    engagemant = None
+
+    def salary_calculator_for_biedronka_cashier(self):
+        if True:
+            print ("dupa")
+
+if __name__ == '__main__':
+    Salary().education_level = 10
+    Salary().work_group = 1
+    Salary().education_level = 1
+
+    Salary().salary_calculator()
